fc029e04a84b9da576bc221da5861fc01b0ffaad
Jun Hong Park <JP@wl-215-37.wpa.claremont.edu>
2014-09-11 10:19:40 -0700

    viewTest project completed.

---
+++ ViewTest/JPSimpleView.h
@@ -0,0 +1,13 @@
+//
+//  JPSimpleView.h
+//  ViewTest
+//
+//  Created by Jun Hong Park on 9/11/14.
+//  Copyright (c) 2014 John Park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPSimpleView : UIView
+
+@end
---
+++ ViewTest/JPSimpleView.m
@@ -0,0 +1,13 @@
+//
+//  JPSimpleView.m
+//  ViewTest
+//
+//  Created by Jun Hong Park on 9/11/14.
+//  Copyright (c) 2014 John Park. All rights reserved.
+//
+
+#import "JPSimpleView.h"
+
+@implementation JPSimpleView
+
+@end
